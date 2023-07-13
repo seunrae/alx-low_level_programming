5dab1fb6f9b1f7887e6c4bd681f9935770335cb9
seunrae <ademilua.oluseun@gmail.com>
2023-07-13 07:36:30 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void){
-  char myString[] = "\"Programming is like building a multilingual puzzle\n";
+  char myString[] = "\"Programming is like building a multilingual puzzle";
   puts(myString);
   return (0);
 }
