8df764808f65bc43758dd7dc62689979ac166e60
seunrae <ademilua.oluseun@gmail.com>
2023-07-13 07:39:10 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int main(void){
+/*this program uses the puts function to print to the console*/
+int main(void)
+{
   char myString[] = "\"Programming is like building a multilingual puzzle";
   puts(myString);
   return (0);
