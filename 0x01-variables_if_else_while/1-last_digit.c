f476ad7769c48f09893a1341bf011f7ede03e38d
seunrae <ademilua.oluseun@gmail.com>
2023-07-14 08:19:40 -0700

    1. The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,32 +13,32 @@ int n;
 char nString[20];
 int length;
 char chNum;
-int lastNumber;
+int lastNum;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-sprintf(nString , "%d" , n);
+sprintf(nString, "%d", n);
 length = strlen(nString);
 chNum = nString[length - 1];
-lastNumber = chNum - '0';
+lastNum = chNum - '0';
 if (n < 0)
 {
-	if(lastNumber == 0)
+	if (lastNum == 0)
 	{
-	printf("Last digit of %d is %d and is 0\n", n, lastNumber);
+	printf("Last digit of %d is %d and is 0\n", n, lastNum);
 	}
-printf("Last digit of %d is -%d and is less than 6 and not 0\n", n, lastNumber);
+printf("Last digit of %d is -%d and is less than 6 and not 0\n", n, lastNum);
 }
-else if (lastNumber > 5)
+else if (lastNum > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, lastNumber);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastNum);
 }
-else if (lastNumber == 0)
+else if (lastNum == 0)
 {
-printf("Last digit of %d is %d and is 0\n", n, lastNumber);
+printf("Last digit of %d is %d and is 0\n", n, lastNum);
 }
-else if (lastNumber < 6 && lastNumber != 0)
+else if (lastNum < 6 && lastNum != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastNumber);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastNum);
 }
 return (0);
 }
