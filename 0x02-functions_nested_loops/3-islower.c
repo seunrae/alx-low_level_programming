9a5f29645393270b4fe7a965005a3f17845d22a5
seunrae <ademilua.oluseun@gmail.com>
2023-07-18 08:09:06 -0700

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,17 +2,17 @@
 #include <ctype.h>
 /**
  * _islower - check the code
- *
+ * c - character parameter to be checked
  * Return: Always 0.
  */
 int _islower(int c)
 {
 	if (islower(c))
 	{
-	return 1;
+	return (1);
 	}
 	else
 	{
-	return 0;
+	return (0);
 	}
 }
