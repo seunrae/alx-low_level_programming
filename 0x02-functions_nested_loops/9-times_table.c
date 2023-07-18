48f4e1013ee04258671f3e2828034465a76fb970
seunrae <ademilua.oluseun@gmail.com>
2023-07-18 13:09:16 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,9 +9,9 @@
 void times_table(void)
 {
 int i, j, result;
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for(j = 0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
 		result = i * j;
 			if (j == 0)
