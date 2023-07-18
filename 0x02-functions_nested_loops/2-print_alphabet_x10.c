be52e53cbfecde9c93fcda7bd26d8db011362c8f
seunrae <ademilua.oluseun@gmail.com>
2023-07-18 07:40:14 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
+ * print_alphabet_x10 - check the code
  *
  * Return: Always 0.
  */
 
 void print_alphabet_x10(void)
 {
-int i = 0 , j;
+int i = 0 ,j;
 	while (i < 10)
 	{
 	j = 'a';
