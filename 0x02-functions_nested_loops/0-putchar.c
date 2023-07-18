ff2c7af140b2f2f4bd310c1608b22e2e77882f06
seunrae <ademilua.oluseun@gmail.com>
2023-07-18 06:50:20 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,14 @@
  */
 int main(void)
 {
-char str[8] = "_putchar";
-int i = 0;
-
-	while (strcmp(str, "\0") != 0)
-	{
-	_putchar(str[i]);
-	i++;	
-	}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
