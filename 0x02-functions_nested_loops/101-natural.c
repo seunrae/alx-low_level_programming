0cb3ea7ef4a563fdf04a33eea97e30d4462a7b85
seunrae <ademilua.oluseun@gmail.com>
2023-07-18 15:27:23 -0700

    13. Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,11 +11,11 @@ int main(void)
 int i, result;
 	for (i = 0; i < 1024; i++)
 	{
-		if(i % 3 == 0 || i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
 		result += i;
 		}
 	}
-       printf("%d\n", result);	
-    return (0);
+printf("%d\n", result);	
+return (0);
 }
