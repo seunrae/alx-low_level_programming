e9c74ef9fb0dca620f747f3d62750c4ac6df9c52
seunrae <ademilua.oluseun@gmail.com>
2023-07-20 06:51:08 -0700

    7. I feel like I am diagonally parked in a parallel universe

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,17 +17,11 @@ int i, j;
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j < i; j++)
 			{
-				if (i == j)
-				{
-				_putchar('\\');
-				}
-				else
-				{
 				_putchar(' ');
-				}
 			}
+		_putchar('\\');
 		_putchar('\n');
 		}
 	}
