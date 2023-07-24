951fa2132543cd847e6993e1500502ce89378a12
seunrae <ademilua.oluseun@gmail.com>
2023-07-24 05:04:23 -0700

    2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
 /**
  * main - check the code 
  *
  * Return: Always 0.
  */
-int main(void)
+int _strlen(char *s)
 {
-return (0);
+int length = strlen(s);
+return (length);
 }
