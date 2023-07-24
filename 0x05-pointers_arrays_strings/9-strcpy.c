4f0b5703c986ad20ac8cbf33a8dbb7654efa1217
seunrae <ademilua.oluseun@gmail.com>
2023-07-24 10:09:17 -0700

    9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,16 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
 /**
- * main - check the code 
+ * *_strcpy - check the code
  *
- * Return: Always 0.
+ * @dest: input parameter
+ * @src: input parameter
  */
-int main(void)
+char *_strcpy(char *dest, char *src)
 {
-return (0);
+char *res;
+strcpy(dest, src);
+res = dest;
+return (res);
 }
