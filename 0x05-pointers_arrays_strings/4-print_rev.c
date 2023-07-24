36330c79dc3d7645fbe4df3abd3c7a64622d3c50
seunrae <ademilua.oluseun@gmail.com>
2023-07-24 05:49:28 -0700

    4. I can only go one way. I've not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,17 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
 /**
  * main - check the code 
  *
  * Return: Always 0.
  */
-int main(void)
+void print_rev(char *s)
 {
-return (0);
+int i;
+	for (i = strlen(s)-1; i >= 0; i--)
+	{
+	_putchar(*(s + i));
+	}
+_putchar('\n');
 }
