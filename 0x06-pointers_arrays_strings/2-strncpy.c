913258d7ae9667240f864188b769cfdbdc2a0a26
seunrae <ademilua.oluseun@gmail.com>
2023-07-26 05:59:46 -0700

    2. strncpy & 3. strcmp

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,8 +2,27 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _strncpy - check the code
+ * @dest: input parameter
+ * @src: input parameter
+ * @n: input parameter
+ * Return: dest.
  */
-
+char *_strncpy(char *dest, char *src, int n)
+{
+char *dest_ptr = dest;
+	while (*src != '\0' && n > 0)
+	{
+	*dest_ptr =  *src;
+	dest_ptr++;
+	src++;
+	n--;
+	}
+	while (n > 0)
+	{
+	*dest_ptr = '\0';
+	dest_ptr++;
+	n--;
+	}
+return (dest);
+}
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-
+int _strcmp(char *s1, char *s2)
+{
+int res = strcmp(s1, s2);
+return (res);
+}
