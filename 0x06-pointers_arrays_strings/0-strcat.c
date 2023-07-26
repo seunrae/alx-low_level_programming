4d7d1390b08c0a22d3b015909d1e1562d1a77bff
seunrae <ademilua.oluseun@gmail.com>
2023-07-26 02:09:28 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,20 +2,21 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _strcat - check the code
+ * @dest: input parameter
+ * @src: input parameter
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
-char *dest_ptr = dest;//hello world!
+char *dest_ptr = dest;
 	while (*dest_ptr != '\0')
 	{
 	dest_ptr++;
 	}
 	while (*src != '\0')
 	{
-	*dest_ptr = *src; 
+	*dest_ptr = *src;
 	dest_ptr++;
 	src++;
 	}
