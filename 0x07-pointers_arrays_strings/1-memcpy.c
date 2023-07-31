df1ae0ee157fb0515f1964942b09bfdeec70a5c7
seunrae <ademilua.oluseun@gmail.com>
2023-07-31 05:51:11 -0700

    1. memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,8 +1,15 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
 /**
- * main - check the code
- *
+ * _memcpy - check the code
+ * @dest: input parameter
+ * @src: input parameter
+ * @n: input parameter
  * Return: Always 0.
  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+memcpy(dest, src, n);
+return (dest);
+}
