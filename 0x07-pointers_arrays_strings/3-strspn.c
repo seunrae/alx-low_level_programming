61540657011031dc4936e72290bfe155778a11ef
seunrae <ademilua.oluseun@gmail.com>
2023-07-31 07:09:20 -0700

    3. strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,8 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int res = strspn(s, accept);
+return (res);
+}
