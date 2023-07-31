fec322f7b247c333fe1005d9db73b6b4a7a5ea49
seunrae <ademilua.oluseun@gmail.com>
2023-07-31 07:32:49 -0700

    4. strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,8 +1,14 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
 /**
- * main - check the code
- *
+ * _strpbrk - check the code
+ * @s: input parameter
+ * @accept: input parameter
  * Return: Always 0.
  */
+char *_strpbrk(char *s, char *accept)
+{
+char *res = strpbrk(s, accept);
+return (res);
+}
