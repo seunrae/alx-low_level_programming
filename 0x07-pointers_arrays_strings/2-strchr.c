740ccbef5dee5e3831fe98be742237cb580bfc22
seunrae <ademilua.oluseun@gmail.com>
2023-07-31 06:50:25 -0700

    2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,8 +1,19 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
 /**
- * main - check the code
+ * _strchr - check the code
+ * @s: input parameter
+ * @c: input parameter
  *
  * Return: Always 0.
  */
+char *_strchr(char *s, char c)
+{
+char *res = strchr(s, c);
+	if (!*res)
+	{
+	return (NULL);
+	}
+return (res);
+}
