871bf571a336f27fbf9a856b93502f4446725544
seunrae <ademilua.oluseun@gmail.com>
2023-07-31 07:49:34 -0700

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,8 +1,14 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
 /**
- * main - check the code
- *
+ * _strstr - check the code
+ * @haystack: input parameter
+ * @needle: input parameter
  * Return: Always 0.
  */
+char *_strstr(char *haystack, char *needle)
+{
+char *res =  strstr(haystack, needle);
+return (res);
+}
