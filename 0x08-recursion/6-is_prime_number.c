5cb61ba5e0a41fef6c6063e4276cf6bb13097f3d
seunrae <ademilua.oluseun@gmail.com>
2023-08-02 13:30:25 -0700

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,9 +1,36 @@
-#include "main.h"
 #include <stdio.h>
-
+#include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * is_prime_number_helper - main code
+ * @n: input parameter
+ * @divisor: input parameter
+ * Return: prime number
  */
-
+int is_prime_number_helper(int n, int divisor)
+{
+	if (divisor <= 1)
+	{
+	return (1);
+	}
+	if (n % divisor == 0)
+	{
+	return (0);
+	}
+	else
+	{
+	return (is_prime_number_helper(n, divisor - 1));
+	}
+}
+/**
+ * is_prime_number - main code
+ * @n: input code
+ * Return: prime number
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+	return (0);
+	}
+return (is_prime_number_helper(n, n - 1));
+}
