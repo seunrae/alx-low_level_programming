7dbf08c51485974f61906930d061ba86b0865914
seunrae <ademilua.oluseun@gmail.com>
2023-08-02 04:25:42 -0700

    1. Why is it so important to dream? Because, in my dreams we are together

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,8 +2,16 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
+ * _print_rev_recursion - check the code
+ * @s: input parameter
  * Return: Always 0.
  */
-
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	return;
+	}
+_print_rev_recursion(s + 1);
+_putchar(*s);	
+}
