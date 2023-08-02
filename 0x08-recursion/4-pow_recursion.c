8944512d8e9a50578775c49e58751d47b2378d06
seunrae <ademilua.oluseun@gmail.com>
2023-08-02 08:36:22 -0700

    4. Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,8 +2,28 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _pow_recursion - check the code
+ * @x: input parameter
+ * @y: input parameter
+ * Return: product of the power of x
  */
-
+int _pow_recursion(int x, int y)
+{
+	if (y == 1)
+	{
+	return (x);
+	}
+	if (y < 0)
+	{
+	return (-1);
+	}
+	if (y == 0)
+	{
+	return (1);
+	}
+	else
+	{
+	y--;
+	return (x * _pow_recursion(x, y));
+	}
+}
