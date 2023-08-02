099ad6b686112d8ca075bf1a6dbf50e756a626fe
seunrae <ademilua.oluseun@gmail.com>
2023-08-02 04:53:48 -0700

    3. You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,8 +2,19 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * factorial - check the code
+ * @n: input parameter
+ * Return: Always product of the factorial of n.
  */
-
+int factorial(int n)
+{
+	if (n < 0)
+	{
+	return (-1);
+	}
+	if (n == 1)
+	{
+	return (1);
+	}
+return (n * factorial(n - 1));
+}
