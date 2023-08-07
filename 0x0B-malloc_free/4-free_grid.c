8d76a0d35159459ff8f791244dc5640c2d22f32b
seunrae <ademilua.oluseun@gmail.com>
2023-08-07 14:27:36 -0700

    4. It's not bragging if you can back it up

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,8 +1,21 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * 
- *
- *
+ * free_grid - accepts a 2d array and frees it
+ * @grid: input parameter
+ * @height: input parameter
  */
+void free_grid(int **grid, int height)
+{
+int i;
+	if (grid == NULL)
+	{
+	return;
+	}
+	for (i = 0; i < height; i++)
+	{
+	free(grid[i]);
+	}
+free(grid);
+}
