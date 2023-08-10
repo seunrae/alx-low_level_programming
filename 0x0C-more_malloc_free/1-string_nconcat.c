95e43df9dad0dc155ab3d9d61308cf0aa56f2650
seunrae <ademilua.oluseun@gmail.com>
2023-08-10 05:22:57 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *ptr = malloc(strlen(s1) + n + 1);
+char *ptr = malloc(sizeof(char) * (strlen(s1) + n) + 1);
 	if (s1 == NULL)
 	{
 	s1 = "";
