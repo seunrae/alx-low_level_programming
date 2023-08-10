55ce3eb24af5a9e7a0245ab71bcb92ef4bcae1cf
seunrae <ademilua.oluseun@gmail.com>
2023-08-10 08:46:40 -0700

    3. array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,8 +1,28 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
- *
- *
- *
+ * array_range - function that returns an array of integers
+ * @min: input parameter
+ * @max: input parameter
+ * Return: pointer to the array
  */
-
+int *array_range(int min, int max)
+{
+int elenum = max - min + 1;
+int *arr = malloc(sizeof(int) * elenum);
+int i;
+	if (min > max)
+	{
+	return (NULL);
+	}
+	if (arr == NULL)
+	{
+	return (NULL);
+	}
+	for (i = min; i < elenum; i++)
+	{
+	arr[i] = i;
+	}
+return (arr);
+}
