45a0b6b35b4f18c838caa95ac77a519fc070e3e7
seunrae <ademilua.oluseun@gmail.com>
2023-08-14 06:28:19 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - structure to hold data for a dog
+ * @name: char value
+ * @age: float value
+ * @owner: char value
+ *
+ */
 struct dog
 {
 char *name;
